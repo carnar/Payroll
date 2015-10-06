de4232b5018ebbb0745bf1a9a64bc1798d8c7aa1
Carlos Narez <cnarez@gmail.com>
2015-10-06 15:58:40 -0600

    test

--- tests/RegularPayrollServiceTest.php
+++ tests/RegularPayrollServiceTest.php
@@ -5,8 +5,24 @@ class RegularPayrollServiceTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_calculates_salary_for_a_regular_employee_who_earns_1000()
+    public function it_calculates_salary_for_a_regular_employee_whose_base_salary_is_1000()
     {
         $employeeMock = $this->getMock('Payroll\EmployeeInterface');
+        $employeeMock->expects($this->once())
+                        ->method('getBaseSalary')
+                        ->will($this->returnValue(1000));
+        $isrServiceMock = $this->getMock('Payroll\EmployeeFeeInterface');
+        $isrServiceMock->expects($this->once())
+                        ->method('fee')
+                        ->will($this->returnValue(50))
+        $loanServiceMock = $this->getMock('Payroll\EmployeeFeeInterface');
+        $loanServiceMock->expects($this->once())
+                            ->method('fee')
+                            ->will($this->returnValue(100));
+        $loanServiceMock = $this->expects($this->once())
+                                ->method('fee')
+                                ->will($this->returnValue(200)); 
+
+                                
     }
 }
