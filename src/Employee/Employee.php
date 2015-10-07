909ef376eb8bb38aed41c9cfd3f19c78c7986fd0
Carlos Narez <cnarez@gmail.com>
2015-10-07 12:42:31 -0600

    Adding few PHP annotations.

--- src/Employee/Employee.php
+++ src/Employee/Employee.php
@@ -5,11 +5,42 @@ use Payroll\Employee\EmployeeInterface;
 
 class Employee implements EmployeeInterface 
 {
+    /**
+     * Employee ID
+     *
+     * @var string
+     */
     private $id;
+
+    /**
+     * Employee's name.
+     *
+     * @var string
+     */
     public $name;
+
+    /**
+     * Employee's base salary.
+     *
+     * @var float
+     */
     private $baseSalary;
+
+    /**
+     * Employee type.
+     *
+     * @var string
+     */
     private $type;
 
+    /**
+     * Make instance of Employee.
+     *
+     * @param string $id
+     * @param string $name
+     * @param float $baseSalary
+     * @param string $type
+     */
     public function __construct($id, $name, $baseSalary, $type)
     {
         $this->id = $id;
@@ -18,11 +49,21 @@ class Employee implements EmployeeInterface
         $this->type = $type;
     }
 
+    /**
+     * Get employee's base salary.
+     *
+     * @return float
+     */
     public function getBaseSalary()
     {
         return $this->baseSalary;
     }
 
+    /**
+     * Get employee type.
+     *
+     * @return string
+     */
     public function type()
     {
         return $this->type;
